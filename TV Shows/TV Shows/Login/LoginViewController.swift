1fd9be32af4f5799f4b6dbd9c3dd5b073d884930
Lora Zubic <lamia.lora@gmail.com>
2022-07-08 14:09:57 +0200

    Remove xcodeproj from gitignore and add Login group

---
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  TV Shows
+//
+//  Created by Infinum on 08.07.2022..
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Test")
+    }
+    
+}
