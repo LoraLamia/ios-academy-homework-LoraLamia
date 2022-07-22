472367ae521f2395b072b433e7b4e7fc696b8d63
Lora Zubic <lamia.lora@gmail.com>
2022-07-22 11:54:12 +0200

    PR change

--- TV Shows/TV Shows/Home/HomeViewController.swift
+++ TV Shows/TV Shows/Home/HomeViewController.swift
@@ -11,7 +11,7 @@ class HomeViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+        super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
