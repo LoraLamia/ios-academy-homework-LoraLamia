95d91ec9064c6f5c2193284d0254ae845264ef6a
Lora Zubic <lamia.lora@gmail.com>
2022-07-22 16:17:14 +0200

    API Added

--- TV Shows/TV Shows/Home/HomeViewController.swift
+++ TV Shows/TV Shows/Home/HomeViewController.swift
@@ -1,7 +1,11 @@
 
 import UIKit
+import Alamofire
+import MBProgressHUD
 
 class HomeViewController: UIViewController {
+    
+    //MARK: Properties
     
     var user: UserResponse?
     var authInfo: AuthInfo?
@@ -10,6 +14,26 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        MBProgressHUD.showAdded(to: view, animated: true)
+        guard let authInfo = authInfo else { return }
+        AF.request(
+              "https://tv-shows.infinum.academy/shows",
+              method: .get,
+              parameters: ["page": "1", "items": "100"], 
+              headers: HTTPHeaders(authInfo.headers)
+          )
+          .validate()
+          .responseDecodable(of: ShowsResponse.self) { [weak self] dataResponse in
+              guard let self = self else { return }
+              MBProgressHUD.hide(for: self.view, animated: true)
+              switch dataResponse.result {
+              case .success(let showsResponse):
+                  print("succes")
+              case .failure:
+                  print("error")
+                
+              }
+          }
 
     }
     
--- TV Shows/TV Shows/Models/Models.swift
+++ TV Shows/TV Shows/Models/Models.swift
@@ -52,3 +52,27 @@ struct AuthInfo: Codable {
     }
 }
 
+struct ShowsResponse: Decodable {
+    let shows: [Show]
+}
+
+struct Show: Decodable {
+    let id: String
+    let title: String
+//    let averageRating: String?
+//    let description: String?
+    let imageUrl: String
+    let noOfReviews: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title
+//        case description
+//        case averageRating = "average_rating"
+        case imageUrl = "image_url"
+        case noOfReviews = "no_of_reviews"
+    }
+}
+
+
+
+
