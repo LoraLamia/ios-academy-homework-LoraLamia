2ec04fe745734498a664a86635fc4f85bfa0034e
Lora Zubic <lamia.lora@gmail.com>
2022-07-23 20:14:00 +0200

    Show reviews API call added

--- TV Shows/TV Shows/ShowDetails/ShowDetailsViewController.swift
+++ TV Shows/TV Shows/ShowDetails/ShowDetailsViewController.swift
@@ -6,17 +6,53 @@
 //
 
 import UIKit
+import Alamofire
+import MBProgressHUD
 
 class ShowDetailsViewController: UIViewController {
     
+    //***Jel showId mora biti optional?
+    
     var authInfo: AuthInfo?
-    var showId: String?
+    var showId: String = ""
     var show: Show?
+    var reviews: [Review] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        fetchReviews()
+    }
+    
+    private func fetchReviews() {
+        guard let authInfo = authInfo else { return }
+        AF.request(
+              "https://tv-shows.infinum.academy/shows/\(showId)/reviews",
+              method: .get,
+              parameters: ["page": "1", "items": "100"],
+              headers: HTTPHeaders(authInfo.headers)
+          )
+          .validate()
+          .responseDecodable(of: ReviewsResponse.self) { [weak self] dataResponse in
+              guard let self = self else { return }
+              MBProgressHUD.hide(for: self.view, animated: true)
+              switch dataResponse.result {
+              case .success(let reviewsResponse):
+                  self.handleSuccesCase(reviewsResponse: reviewsResponse)
+              case .failure:
+                  print("error")
+                
+              }
+          }
+    }
+    
+    private func handleSuccesCase(reviewsResponse: ReviewsResponse) {
+        reviews = reviewsResponse.reviews
+        print("succes")
+    }
+    
+    private func handleErrorCase() {
+        print("error")
     }
     
 
