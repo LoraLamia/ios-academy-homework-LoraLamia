06dd3d5b84272c67edfacac1ae7fcc5e7d50f652
Lora Zubic <lamia.lora@gmail.com>
2022-07-31 14:14:16 +0200

    HW6 Finished

--- TV Shows/TV Shows/SceneDelegate.swift
+++ TV Shows/TV Shows/SceneDelegate.swift
@@ -11,21 +11,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+
+        let navigationController = UINavigationController()
         
-        if let savedAuthInfo = UserDefaults.standard.object(forKey: UserDefaultsKeys.authInfo.rawValue) as? Data {
-            let decoder = JSONDecoder()
-            if let auth2 = try? decoder.decode(AuthInfo.self, from: savedAuthInfo) {
-                print(auth2)
-            }
+        guard let savedAuthInfo = UserDefaults.standard.object(forKey: UserDefaultsKeys.authInfo.rawValue) as? Data else { return }
+        let decoder = JSONDecoder()
+        let authInfo = try? decoder.decode(AuthInfo.self, from: savedAuthInfo)
+
+        if authInfo != nil {
+            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+            let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+            homeViewController.authInfo = authInfo
+            navigationController.viewControllers = [homeViewController]
+        } else {
+            let storyboard = UIStoryboard(name: "Login", bundle: nil)
+            let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+            navigationController.viewControllers = [loginViewController]
         }
         
-        
-        guard let _ = (scene as? UIWindowScene) else { return }
+        window?.rootViewController = navigationController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
